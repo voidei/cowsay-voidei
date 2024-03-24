0d69fb5c159883bace208e9ec1a297fb632ac405
Dawn Walker <dawniepieuwu@gmail.com>
2024-03-25 04:25:05 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import sys
 import textwrap
 
-from thecow import check_input, constantine, increase_character
+import thecow as cow
 
-check_input(sys.argv)
+cow.check_input(sys.argv)
 
 params = sys.argv[1]
 """What the cow says."""
@@ -18,30 +18,30 @@ def cowsay(input: str):
     wrapped_text = textwrap.wrap(input, width=39, break_long_words=False)
 
     if len(wrapped_text) == 1:
-        underscore = increase_character("_", 0, length)
-        dash = increase_character("-", 0, length)
-        say = f""" {underscore}\n< {input} >\n {dash}{constantine}"""
+        underscore = cow.increase_character("_", 0, length)
+        dash = cow.increase_character("-", 0, length)
+        say = f""" {underscore}\n< {input} >\n {dash}{cow.constantine}"""
 
     if len(wrapped_text) == 2:
         max_length = len(wrapped_text[0])
-        underscore = increase_character("_", 0, max_length)
-        dash = increase_character("-", 0, max_length)
+        underscore = cow.increase_character("_", 0, max_length)
+        dash = cow.increase_character("-", 0, max_length)
         say = f"""\
  {underscore}
 / {wrapped_text[0]} \\
 \\ {wrapped_text[1]:<39} /
- {dash}{constantine}"""
+ {dash}{cow.constantine}"""
 
     if len(wrapped_text) >= 3:
         max_length = len(wrapped_text[0])
-        underscore = increase_character("_", 0, max_length)
-        dash = increase_character("-", 0, max_length)
+        underscore = cow.increase_character("_", 0, max_length)
+        dash = cow.increase_character("-", 0, max_length)
         say = f"""\
  {underscore}
 / {wrapped_text[0]} \\
 | {wrapped_text[1]:<39} |
 \\ {wrapped_text[2]:<39} /
- {dash}{constantine}"""
+ {dash}{cow.constantine}"""
 
     print(say)
 
